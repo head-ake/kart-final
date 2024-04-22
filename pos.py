291d10cc60efb3a4cf1520c93a4fbe11c50a0eaf
Mik Eyer <headake@miks-mbp.uconnect.utah.edu>
2024-04-22 10:35:38 -0600

    pos

---
+++ pos.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+import cv2
+import numpy as np
+
+from ultralytics import YOLO
+
+model = YOLO('yolov8n.pt')
+
+video_path = 'path'
+cap = cv2.VideoCapture(video_path)
+
+track_history = defaultdict(lambda: [])
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if ret:
+        results = model.track(frame, persist=True)
+
+        annotated_frame = results[0].plot()
+
+        cv2.imshow("tracking", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
