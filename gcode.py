cd06ceb55e27bdccf9af16356b7760a1f2f103bf
TEIGEN EDMUNDSON <u1296323@umail.utah.edu>
2024-04-26 09:21:52 -0600

    crren

--- gcode.py
+++ gcode.py
@@ -6,10 +6,12 @@ baud = 115200
 cereal = serial.Serial(printer_port, baud, timeout=1)
 home = "G28"
 
+outfile_path = 'file.gcode'
+
 def getGCode(toFile: bool, list):
     WRITINGDEPTH = 0
     MOVINGDEPTH = 15
-    g = G(outfile=r'D:/Turner-Barbour/kart-final/file.gcode')
+    g = G(outfile=outfile_path)
 
     def circle(point, size):
         g.abs_move(point[0]-size, point[1], MOVINGDEPTH)
@@ -37,7 +39,7 @@ def getGCode(toFile: bool, list):
         g.abs_move(x[0], x[1], MOVINGDEPTH)
 
 def outputFile():
-    with open('D:/Turner-Barbour/kart-final/file.gcode', 'r') as f:
+    with open(outfile_path, 'r') as f:
         for line in f:
             print(line)
             cereal.write(line.encode('utf-8'))
